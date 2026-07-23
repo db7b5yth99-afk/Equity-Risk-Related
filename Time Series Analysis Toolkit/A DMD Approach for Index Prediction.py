7ada2eed1282bdecde88b967d7afbebf87ec47b4
A.Li <db7b5yth99@privaterelay.appleid.com>
2026-07-23 09:31:28 +0800

    Refactor DMD prediction with normalization
    Refactor DMD prediction function to normalize data before prediction and adjust output back to original scale.

--- Time Series Analysis Toolkit/A DMD Approach for Index Prediction.py
+++ Time Series Analysis Toolkit/A DMD Approach for Index Prediction.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 tickers = [
-    "^GSPC",                                     # Benchmark
+    "^GSPC",                                     # Target
     "XLK", "XLF", "XLV", "XLY", "XLI", "XLE", "XLB", "XLU", # Sectors
     "MTUM", "VLUE", "QUAL", "SPLV", "IWM",     # Smart Beta Factors
     "TLT", "IEF", "GLD", "UUP", "USO"          # Cross-Asset Macro
@@ -14,23 +14,32 @@ pct = 1 + price.pct_change().dropna(how='any')
 def dmd_pred(df, t=None):
     if t is None:
         t=252*5
-    df5y = df.iloc[-t:]
-    df5y_utility = df.iloc[-t-1:-1]
+        
+    window = df.iloc[-t-1:]
+    mean = window.mean()
+    std = window.std()
+    df5y = (df.iloc[-t:] - mean) / std
+    df5y_utility = (df.iloc[-t-1:-1] - mean) / std
     X = df5y_utility.to_numpy().T
     Y = df5y.to_numpy().T
     x_latest = Y[:, 0]
     X_inverse = np.linalg.pinv(X)
+    
     A = Y @ X_inverse
     U, Sigma, Vt = np.linalg.svd(X, full_matrices=False)
     A_tilde = U.T @ A @ U
     lmd, W = np.linalg.eig(A_tilde)
     phi = Y @ Vt.T @ np.diag(1.0 / Sigma) @ W
     b = np.linalg.pinv(phi) @ x_latest
-    X_pred = phi @ np.diag(lmd) @ b
+    X_pred = phi @ np.diag(lmd) @ b    
+    X_pred = (X_pred * std.to_numpy()) + mean.to_numpy()
     spy_idx = list(df.columns).index("^GSPC")
+    
     return X_pred[spy_idx].real
 
-print(f'S&P 500 Last Close Price: {price['^GSPC'].iloc[-1]:.2f}')
+
+
+print(f"S&P 500 Last Close Price: {price['^GSPC'].iloc[-1]:.2f}")
 print('DMD Prediction of S&P 500 in 1-d')
 ret1 = price['^GSPC'].iloc[-1] * dmd_pred(df=pct, t=5)
 ret2 = price['^GSPC'].iloc[-1] * dmd_pred(df=pct, t=21)
